06fc690a13e92ec321929b77f450b19e02640ff1
billyagasi <billy.agasi@btpn.com>
2023-11-29 09:43:34 +0700

    testing-5

--- internal/model/indexinfo.go
+++ internal/model/indexinfo.go
@@ -1,8 +1,8 @@
-// model/indexinfo.go
 package model
 
 type IndexInfo struct {
 	IndexName string
+	StoreSize string // Assuming StoreSize is a string
 	SizeKB    float64
 	Status    string
 	Health    string
